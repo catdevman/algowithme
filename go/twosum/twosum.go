1f871e9398cecd1dc7a07c3c6d8df820e3e76fe2
Lucas Pearson <catdevman@gmail.com>
2024-03-16 14:47:33 -0400

    remove faulty algo solution

--- go/twosum/twosum.go
+++ go/twosum/twosum.go
@@ -2,12 +2,5 @@ package twosum
 
 func TwoSum(nums []int, target int) []int {
 	out := []int{}
-	for i := 0; i < len(nums)-1; i++ {
-		if nums[i]+nums[i+1] == target {
-			out = append(out, i)
-			out = append(out, i+1)
-			break
-		}
-	}
 	return out
 }
