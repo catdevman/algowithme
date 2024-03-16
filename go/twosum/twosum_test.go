c246b93259263b04564488520dd9b4fecfa12c5a
Lucas Pearson <catdevman@gmail.com>
2024-03-16 14:42:36 -0400

    add first problem twosum

---
+++ go/twosum/twosum_test.go
@@ -0,0 +1,38 @@
+package twosum
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		nums     []int
+		target   int
+		expected []int
+	}{
+		{
+			[]int{2, 7, 11, 15},
+			9,
+			[]int{0, 1},
+		},
+		{
+			[]int{3, 2, 4},
+			6,
+			[]int{1, 2},
+		},
+	}
+
+	for _, test := range tests {
+		out := TwoSum(test.nums, test.target)
+		if len(out) != len(test.expected) {
+			t.Fatal(fmt.Sprintf("output of TwoSum had len %d but expected has len %d", len(out), len(test.expected)))
+		}
+		for _, expected := range test.expected {
+			if !slices.Contains[[]int](out, expected) {
+				t.Fatal(fmt.Sprintf("output does not have %d like it should", expected))
+			}
+		}
+	}
+}
