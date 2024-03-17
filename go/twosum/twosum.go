0171d09e77fbc6c20cac3ebe4245f6c32f52393b
Lucas Pearson <catdevman@gmail.com>
2024-03-17 12:59:35 -0400

    created solution

--- go/twosum/twosum.go
+++ go/twosum/twosum.go
@@ -2,5 +2,15 @@ package twosum
 
 func TwoSum(nums []int, target int) []int {
 	out := []int{}
+outerLoop:
+	for i := 0; i <= len(nums)-2; i++ {
+		for j := i + 1; j <= len(nums)-1; j++ {
+			if nums[i]+nums[j] == target {
+				out = append(out, i)
+				out = append(out, j)
+				break outerLoop
+			}
+		}
+	}
 	return out
 }
