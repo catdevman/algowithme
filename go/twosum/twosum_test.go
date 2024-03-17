2372fa08c5777ff62f4404c0416eb75e50924500
Lucas Pearson <catdevman@gmail.com>
2024-03-17 17:52:10 -0400

    small test updates

--- go/twosum/twosum_test.go
+++ go/twosum/twosum_test.go
@@ -7,37 +7,34 @@ import (
 )
 
 func TestTwoSum(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		nums     []int
 		target   int
 		expected []int
 	}{
-		{
+		"first two sum to target": {
 			[]int{2, 7, 11, 15},
 			9,
 			[]int{0, 1},
 		},
-		{
+		"last two sum to target": {
 			[]int{3, 2, 4},
 			6,
 			[]int{1, 2},
 		},
-		{
-			[]int{2, 3, 4},
-			6,
-			[]int{0, 2},
-		},
 	}
 
-	for _, test := range tests {
-		out := TwoSum(test.nums, test.target)
-		if len(out) != len(test.expected) {
-			t.Fatal(fmt.Sprintf("output of TwoSum had len %d but expected has len %d", len(out), len(test.expected)))
-		}
-		for _, expected := range test.expected {
-			if !slices.Contains[[]int](out, expected) {
-				t.Fatal(fmt.Sprintf("output does not have %d like it should", expected))
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			out := TwoSum(test.nums, test.target)
+			if len(out) != len(test.expected) {
+				t.Fatal(fmt.Sprintf("output of TwoSum had len %d but expected has len %d", len(out), len(test.expected)))
 			}
-		}
+			for _, expected := range test.expected {
+				if !slices.Contains[[]int](out, expected) {
+					t.Fatal(fmt.Sprintf("output does not have %d like it should", expected))
+				}
+			}
+		})
 	}
 }
