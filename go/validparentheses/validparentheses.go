467a18ee4f7da05e1d555dbc2106e77e4391a52e
Lucas Pearson <catdevman@gmail.com>
2024-09-26 13:35:26 -0400

    add solution

--- go/validparentheses/validparentheses.go
+++ go/validparentheses/validparentheses.go
@@ -1,5 +1,22 @@
 package validparentheses
 
 func ValidParentheses(input string) bool {
-	return false
+	stack := []rune{}
+	mapping := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	for _, char := range input {
+		if char == '(' || char == '{' || char == '[' {
+			stack = append(stack, char)
+		} else {
+			if len(stack) == 0 || stack[len(stack)-1] != mapping[char] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
 }
